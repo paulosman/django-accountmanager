369b23ef0ef0fa086a7d9a5b37097f53a28a6a3c
Paul Osman <paul@eval.ca>
2010-08-17 21:23:24 -0400

    Remember how dicts work?

--- accountmanager/middleware.py
+++ accountmanager/middleware.py
@@ -4,9 +4,9 @@ class StatusHeader(object):
         if not request.user.is_authenticated():
             response['X-Account-Management-Status'] = 'none'
             return response
-        status = 'active; id="%(username)s"; name="%(name)"' % {
-            username: request.user.username,
-            name: request.user.get_full_name()
+        status = 'active; id="%(username)s"; name="%(name)s"' % {
+            'username': request.user.username,
+            'name': request.user.get_full_name()
         }
         response['X-Account-Management-Status'] = status
         return response
